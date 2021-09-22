607782ded815f24e0449cecb41f325359e0c50fa
KK <kkashiva@gmail.com>
2021-09-23 00:50:27 +0530

    terrnary condition for duration color

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import './session.dart';
@@ -59,20 +60,46 @@ class MyHomePage extends StatelessWidget {
                     child: Row(
                       children: <Widget>[
                         Container(
-                          child: Text(s.sessionDuration().toString()),
-                          decoration: BoxDecoration(),
+                          child: Text(
+                            s.sessionDuration().toString(),
+                            style: TextStyle(
+                                color: Color(0xffffffff), fontSize: 22),
+                          ),
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: (s.sessionDuration() < 60)
+                                ? Colors.lightGreen.shade300
+                                : (s.sessionDuration() < 120)
+                                    ? Colors.green.shade500
+                                    : Color(0xff23a455),
+                          ),
+                          padding: EdgeInsets.all(10),
+                          margin:
+                              EdgeInsets.symmetric(horizontal: 15, vertical: 5),
                         ),
-                        Column(
-                          children: [
-                            Text(s.location),
-                            Text(DateFormat.E().format(s.start)),
-                          ],
+                        Container(
+                          child: Column(
+                            children: [
+                              Text(
+                                s.location,
+                                style: TextStyle(fontSize: 20),
+                              ),
+                              Text(DateFormat.E().format(s.start),
+                                  style: TextStyle(
+                                      fontSize: 16, color: Colors.grey)),
+                            ],
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                          ),
+                          margin: EdgeInsets.symmetric(horizontal: 15),
+                          width: 200,
                         ),
                       ],
                       crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     ),
                     width: double.infinity,
+                    height: 60,
                   ),
                 );
               }).toList(),
