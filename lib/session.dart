eb30b7c1a1e677506e8f25e613ee79c853cfad68
KK <kkashiva@gmail.com>
2021-09-22 23:06:05 +0530

    mapped session data into cards list

---
+++ lib/session.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/foundation.dart';
+
+class Session {
+  final String id;
+  final String location;
+  final DateTime start;
+  final DateTime end;
+  Session(
+      {required this.id,
+      required this.location,
+      required this.start,
+      required this.end});
+  Duration sessionDuration() {
+    var sessionDuration = end.difference(start);
+    return sessionDuration;
+  }
+}
